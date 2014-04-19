0b686544e939e306b71d0d9d2b67e6fe5612275b
d3dev <d3dev.github@gmail.com>
2014-04-19 14:50:05 +0200

    Web portal becomes useless since closure of auction house. www folder deleted.

--- www/sell.php
+++
@@ -1,8 +0,0 @@
-<?php
-$count = 0;
-$db = new PDO('sqlite:../items.db');
-$id = $_POST['id'];
-$price = $_POST['price'];
-if (is_numeric($id) && is_numeric($price)) $count = $db->exec("UPDATE d3_custom SET sold=$price WHERE id=$id");
-echo $count;
-?>
